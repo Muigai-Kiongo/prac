0dc58b469dde1f3649736eae7e8a1b7195555e2e
Muigai-Kiongo <kabochakiongo@gmail.com>
2026-09-18 20:41:30 +0300

    add data

--- src/data/items.js
+++ src/data/items.js
@@ -3,5 +3,15 @@ export const shoppingList =[
         {id: 2, name: 'Salt'},
         {id: 3, name: 'Chair'},
         {id: 4, name: 'Shampoo'},
+        {id: 5, name: 'Table'},
+        {id: 1, name: 'Sugar'},
+        {id: 2, name: 'Salt'},
+        {id: 3, name: 'Chair'},
+        {id: 4, name: 'Shampoo'},
+        {id: 5, name: 'Table'},
+        {id: 1, name: 'Sugar'},
+        {id: 2, name: 'Salt'},
+        {id: 3, name: 'Chair'},
+        {id: 4, name: 'Shampoo'},
         {id: 5, name: 'Table'}
     ]
